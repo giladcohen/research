0ce5bf9b48b91766765b3e3e76d29291c15952d6
giladcohen <giladadlig@gmail.com>
2021-10-10 05:09:57 +0300

    using bugging gradients

--- scripts/extract_characteristics.py
+++ scripts/extract_characteristics.py
@@ -422,7 +422,7 @@ if args.detect_method == 'mahalanobis':
     logger.info('Done calculating: sample_mean, precision.')
 
     if args.magnitude == -1:
-        magnitude_vec = [0.000000001]
+        magnitude_vec = [0.000000001, 0.00000001, 0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1]
     else:
         magnitude_vec = [args.magnitude]
 
