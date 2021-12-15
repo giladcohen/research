b3ce5f6598c14e0d0bdbe2da69e9af32670b603d
giladcohen <giladadlig@gmail.com>
2021-12-16 00:52:39 +0200

    add encoder decoder wrapper

--- scripts/eval_seg.py
+++ scripts/eval_seg.py
@@ -40,6 +40,7 @@ timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
 json_file = os.path.join(METRICS_DIR, 'eval_{}.json'.format(timestamp))
 
 # build the dataloader
+cfg.data.test['type'] = 'MyPascalVOCDataset'
 dataset = build_dataset(cfg.data.test)
 data_loader = build_dataloader(
     dataset,
@@ -78,8 +79,8 @@ results = []
 prog_bar = mmcv.ProgressBar(len(dataset))
 
 # debug
-# batch_idx = 0
-# data = list(data_loader)[0]
+batch_idx = 0
+data = list(data_loader)[0]
 for batch_idx, data in enumerate(data_loader):
     out = wrapper(data)
     result = out['preds'].cpu().numpy()
