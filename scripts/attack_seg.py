d236e18752e1efb8c11979d727c29d4cce183b69
giladcohen <giladadlig@gmail.com>
2021-12-19 22:38:28 +0200

    refactor code

--- scripts/attack_seg.py
+++ scripts/attack_seg.py
@@ -33,7 +33,7 @@ parser.add_argument('--attack_loss', default='cross_entropy', type=str,
 parser.add_argument('--attack_dir', default='debug2', type=str, help='attack directory')
 
 # for FGSM/PGD:
-parser.add_argument('--eps'     , default=8/255, type=float, help='maximum Linf deviation from original image')
+parser.add_argument('--eps'     , default=0.031, type=float, help='maximum Linf deviation from original image')
 parser.add_argument('--eps_step', default=0.003, type=float, help='step size of each adv iteration')
 
 parser.add_argument('--mode', default='null', type=str, help='to bypass pycharm bug')
