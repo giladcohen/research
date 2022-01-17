71eb48d6ad533b3befe946c30d112f7a49afc6c7
giladcohen <giladadlig@gmail.com>
2022-01-17 02:22:44 +0200

    update attack seg script

--- scripts/attack_seg.py
+++ scripts/attack_seg.py
@@ -135,8 +135,8 @@ def generate_fgsm(wrapper, x, meta, targets):
     return x_adv
 
 # debug
-batch_idx = 4
-data = list(data_loader)[4]
+# batch_idx = 4
+# data = list(data_loader)[4]
 
 for batch_idx, data in enumerate(data_loader):
     targets = data['gt_semantic_seg']
