811a6e5ba0195614a869efd076a5787c1755782a
giladcohen <giladadlig@gmail.com>
2022-05-06 01:06:30 +0300

    plotting attack results

---
+++ plots/tiny_imagenet_attack_comparison.py
@@ -0,0 +1,78 @@
+"""Plotting the MI attack results for CIFAR10"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+datasets = ['cifar10', 'cifar100', 'svhn']
+target_models = ['2', '3', '4', '5', '6', '7']
+attacks  = ['Gap', 'Black-box', 'Boundary dist', 'SIF']
+
+dataset = datasets[2]
+n_groups = len(target_models)
+
+attack_score_dict = {dataset: {
+    '2': {'Gap': 0.979,   'Black-box': 0.929,   'Boundary dist': 0.984, 'SIF': 0.985},   # 1k
+    '3': {'Gap': 0.9526,  'Black-box': 0.9784,  'Boundary dist': 0.988, 'SIF': 0.9972},  # 5k
+    '4': {'Gap': 0.9276,  'Black-box': 0.9877,  'Boundary dist': 0.982, 'SIF': 0.9944},  # 10k
+    '5': {'Gap': 0.904,   'Black-box': 0.986933333333333,  'Boundary dist': 0.975, 'SIF': 0.9958},  # 15k
+    '6': {'Gap': 0.88975, 'Black-box': 0.9747,  'Boundary dist': 0.966, 'SIF': 0.9828},  # 20k
+    '7': {'Gap': 0.87696, 'Black-box': 0.97808, 'Boundary dist': 0.971, 'SIF': 0.96388},  # 25k
+}}
+
+fig, ax = plt.subplots(figsize=(5, 5))
+index = np.arange(n_groups)
+bar_width = 0.15
+opacity = 0.4
+
+# attack rectangle: all of Gap
+values1 = [attack_score_dict[dataset]['2']['Gap'],
+           attack_score_dict[dataset]['3']['Gap'], attack_score_dict[dataset]['4']['Gap'],
+           attack_score_dict[dataset]['5']['Gap'], attack_score_dict[dataset]['6']['Gap'],
+           attack_score_dict[dataset]['7']['Gap']]
+rects1 = plt.bar(index + bar_width, values1, bar_width,
+                 alpha=opacity,
+                 color='black',
+                 edgecolor='black',
+                 label='Gap')
+
+values2 = [attack_score_dict[dataset]['2']['Black-box'],
+           attack_score_dict[dataset]['3']['Black-box'], attack_score_dict[dataset]['4']['Black-box'],
+           attack_score_dict[dataset]['5']['Black-box'], attack_score_dict[dataset]['6']['Black-box'],
+           attack_score_dict[dataset]['7']['Black-box']]
+rects2 = plt.bar(index + 2*bar_width, values2, bar_width,
+                 alpha=opacity,
+                 color='blue',
+                 edgecolor='black',
+                 label='Black-box')
+
+values3 = [attack_score_dict[dataset]['2']['Boundary dist'],
+           attack_score_dict[dataset]['3']['Boundary dist'], attack_score_dict[dataset]['4']['Boundary dist'],
+           attack_score_dict[dataset]['5']['Boundary dist'], attack_score_dict[dataset]['6']['Boundary dist'],
+           attack_score_dict[dataset]['7']['Boundary dist']]
+rects3 = plt.bar(index + 3*bar_width, values3, bar_width,
+                 alpha=opacity,
+                 color='green',
+                 edgecolor='black',
+                 label='Boundary dist')
+
+values4 = [attack_score_dict[dataset]['2']['SIF'],
+           attack_score_dict[dataset]['3']['SIF'], attack_score_dict[dataset]['4']['SIF'],
+           attack_score_dict[dataset]['5']['SIF'], attack_score_dict[dataset]['6']['SIF'],
+           attack_score_dict[dataset]['7']['SIF']]
+rects4 = plt.bar(index + 4*bar_width, values4, bar_width,
+                 alpha=opacity,
+                 color='red',
+                 edgecolor='black',
+                 label='SIF (ours)')
+
+plt.xlabel('Target Model $\mathcal{M}$')
+plt.ylabel('Balanced Acc')
+plt.ylim(bottom=0.87, top=1.002)
+plt.xticks(index + 2.5*bar_width, ('2', '3', '4', '5', '6', '7'))
+plt.yticks([0.9, 0.95, 1.0])
+# plt.legend((rects1, rects2, rects3, rects4), ('Gap', 'Black-box', 'Boundary dist', 'SIF (ours)'),
+#            loc=(0.63, 0.77), ncol=1, fancybox=True, prop={'size': 10})
+plt.tight_layout()
+# plt.show()
+plt.savefig('tiny_imagenet_attack_scores.png', dpi=350)
+
